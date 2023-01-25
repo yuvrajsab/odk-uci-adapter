488ccb4ef628f4948a1ef4507b04adfbf0500a18
Abhishek Mishra <abhishek@luezoid.com>
2023-01-25 12:41:50 +0530

    Tests: fixes

--- src/sms-adapter/cdac/cdac.service.spec.ts
+++ src/sms-adapter/cdac/cdac.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CdacService } from './cdac.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 describe('CdacService', () => {
   let service: CdacService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, HttpModule],
       providers: [CdacService],
     }).compile();
 
--- src/sms-adapter/uci/uci.service.spec.ts
+++ src/sms-adapter/uci/uci.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UciService } from './uci.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 describe('UciService', () => {
   let service: UciService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, HttpModule],
       providers: [UciService],
     }).compile();
 
